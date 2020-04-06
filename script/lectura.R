671e9a01e203f116cb19d3cc89227c9724422ea2
jrlacalle <jralexster@gmail.com>
2020-04-06 23:55:18 +0200

    Primeras pruebas para preparar las gráficas

--- script/lectura.R
+++ script/lectura.R
@@ -1,2 +1,5 @@
 # Lectura de datos desde web ISCIII
 covid_historico <- read.csv("https://covid19.isciii.es/resources/serie_historica_acumulados.csv")
+# Se eliminan las dos últimas filas con comentarios
+covid_es <- covid_historico[1:(nrow(covid_historico)-2),]
+covid_es$Fecha <- as.Date(covid_es$Fecha,"%d/%m/%y")
